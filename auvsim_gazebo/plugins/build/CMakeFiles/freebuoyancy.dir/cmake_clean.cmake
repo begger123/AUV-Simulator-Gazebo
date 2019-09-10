7772d5ebd7607adcf703d833dc1eb09f97686ac3
lafith <mmlafith3@gmail.com>
2019-09-10 15:16:04 +0530

    auv model and plugins added

---
+++ auvsim_gazebo/plugins/build/CMakeFiles/freebuoyancy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/freebuoyancy.dir/freebuoyancy.cc.o"
+  "libfreebuoyancy.pdb"
+  "libfreebuoyancy.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/freebuoyancy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
